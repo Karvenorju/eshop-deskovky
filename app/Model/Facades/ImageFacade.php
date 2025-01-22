188149ed33442cab3534d2ea2601a264701c0a2d
Alexandr Belcenko <bela08@vse.cz>
2025-01-23 00:05:23 +0100

    img jde updatovat

--- app/Model/Facades/ImageFacade.php
+++ app/Model/Facades/ImageFacade.php
@@ -31,27 +31,31 @@ class ImageFacade {
      */
     public function saveProductPhoto(FileUpload $fileUpload, Product $product, ImageType $imageType = ImageType::OTHER): void {
         if ($fileUpload->isOk() && $fileUpload->isImage()) {
-            $image = new Image();
-            $image->product = $product;
-            switch ($imageType) {
-                case ImageType::FRONT:
-                    $image->type = 'front';
-                    $image->url = $product->url . '-front.jpg';
-                    break;
-                case ImageType::BACK:
-                    $image->type = 'back';
-                    $image->url = $product->url . '-back.jpg';
-                    break;
-                case ImageType::BOARD:
-                    $image->type = 'board';
-                    $image->url = $product->url . '-board.jpg';
-                    break;
-                case ImageType::OTHER:
-                    $image->type = 'other';
-                    $image->url = $product->url . '-other.jpg';
-                    break;
+            try {
+                $image = $this->imageRepository->findBy(['product_id' => $product->productId, 'type' => $imageType]);
+            } catch (\Exception $e) {
+                $image = new Image();
+                $image->product = $product;
+                switch ($imageType) {
+                    case ImageType::FRONT:
+                        $image->type = 'front';
+                        $image->url = $product->url . '-front.jpg';
+                        break;
+                    case ImageType::BACK:
+                        $image->type = 'back';
+                        $image->url = $product->url . '-back.jpg';
+                        break;
+                    case ImageType::BOARD:
+                        $image->type = 'board';
+                        $image->url = $product->url . '-board.jpg';
+                        break;
+                    case ImageType::OTHER:
+                        $image->type = 'other';
+                        $image->url = $product->url . '-other.jpg';
+                        break;
+                }
+                $this->imageRepository->persist($image);
             }
-            $this->imageRepository->persist($image);
             $fileUpload->move(__DIR__ . '/../../../www/img/products/' . $image->url);
         }
     }
