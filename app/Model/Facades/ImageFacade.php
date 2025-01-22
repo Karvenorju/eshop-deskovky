a45ac8eb192e280aaf217ee473da538ad73671b8
Alexandr Belcenko <bela08@vse.cz>
2025-01-22 20:41:16 +0100

    image uploading

--- app/Model/Facades/ImageFacade.php
+++ app/Model/Facades/ImageFacade.php
@@ -4,7 +4,9 @@ namespace App\Model\Facades;
 
 use App\Model\Entities\Image;
 use App\Model\Entities\Product;
+use App\Model\Enums\ImageType;
 use App\Model\Repositories\ImageRepository;
+use Nette\Http\FileUpload;
 
 class ImageFacade {
     private ImageRepository $imageRepository;
@@ -19,6 +21,40 @@ class ImageFacade {
 
     public function getFrontImage(Product $product): Image {
         return $this->imageRepository->findBy(['product_id'=>$product->productId,'type'=>'front']);
+    }
+
+    /**
+     * Metoda pro uložení fotky produktu
+     * @param FileUpload $fileUpload
+     * @param Product $product
+     * @throws \Exception
+     */
+    public function saveProductPhoto(FileUpload $fileUpload, Product &$product, ImageType $imageType = ImageType::OTHER): void {
+        //TODO rework this for new photo architecture
+        if ($fileUpload->isOk() && $fileUpload->isImage()) {
+            $image = new Image();
+            $image->product = $product;
+            switch ($imageType) {
+                case ImageType::FRONT:
+                    $image->type = 'front';
+                    $image->url = $product->url . '-front.jpg';
+                    break;
+                case ImageType::BACK:
+                    $image->type = 'back';
+                    $image->url = $product->url . '-back.jpg';
+                    break;
+                case ImageType::BOARD:
+                    $image->type = 'board';
+                    $image->url = $product->url . '-board.jpg';
+                    break;
+                case ImageType::OTHER:
+                    $image->type = 'other';
+                    $image->url = $product->url . '-other.jpg';
+                    break;
+            }
+            $this->imageRepository->persist($image);
+            $fileUpload->move(__DIR__ . '/../../../www/img/products/' . $image->url);
+        }
     }
 
 }
